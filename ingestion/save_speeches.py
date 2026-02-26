3f6b720ee2412c4036724ebfc98106ab3114b1f5
Lennart Rosenthal <rosenthal.lennart@gmail.com>
2026-02-26 17:15:21 +0100

    Create save_speeches_to_parquet function that takes a validated dataframe and output path as inputs, creates output directory if missing, raises error if file exists, and saves as snappy-compressed parquet.

---
+++ ingestion/save_speeches.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+def save_speeches_to_parquet(validated_dataframe: pd.DataFrame, output_path:str):
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    if os.path.exists(output_path):
+        raise FileExistsError(f"File already exists: {e}")
+
+    validated_dataframe.to_parquet(output_path,
+                                    engine='pyarrow',
+                                    compression='snappy',
+                                    index=False)
+    print(f"Saved to: {output_path}")
+
+    return output_path
