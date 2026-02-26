7b5c72d1a3af163bffb436054ccc7a7ad208b17b
Lennart Rosenthal <rosenthal.lennart@gmail.com>
2026-02-26 17:17:43 +0100

    Add a clean comments function to clean the comments text, as we did before with all other text columns.

--- ingestion/cleaning.py
+++ ingestion/cleaning.py
@@ -4,13 +4,32 @@ from pandas import DataFrame
 import re
 
 
-def clean_speech_text(text: str) -> str:
+def clean_text(text: str) -> str:
 
     text = text.replace("\xa0", " ")
     text = text.replace("\u202f", " ")
     text = text.replace("\u2001", " ")
     text = re.sub(r"\s+", " ", text)
     return text.strip()
+
+def clean_comments(comments_list: list)-> list:
+    # each comment is a list of dictionaries
+    # containing the index position of a comments as the key
+    # the comment itself as the value
+    if not isinstance(comments_list, list):
+        return comments_list
+
+    for comment_dict in comments_list:
+
+        if not isinstance(comment_dict,dict):
+            return comment_dict
+        comment_text = comment_dict["comment_text"]
+        cleaned_comment_text = clean_text(comment_text)
+        comment_dict["comment_text"] = cleaned_comment_text
+    return comments_list
+
+
+
 
 def cleaning_raw_speeches(dataframe: pd.DataFrame) -> pd.DataFrame:
 
@@ -25,15 +44,17 @@ def cleaning_raw_speeches(dataframe: pd.DataFrame) -> pd.DataFrame:
                       'speech']
 
     df_text_cleaned = raw_df.copy()
-    df_text_cleaned[string_columns] = df_text_cleaned[string_columns].map(clean_speech_text, na_action="ignore")
+    df_text_cleaned[string_columns] = df_text_cleaned[string_columns].map(clean_text, na_action="ignore")
+    df_text_comments_cleaned = df_text_cleaned.copy()
+    df_text_comments_cleaned["comments"] = df_text_cleaned["comments"].map(clean_comments, na_action="ignore")
 
     # manually convert dtypes, mainly ints and datetime
-    df_text_clean_dtypes_adjusted = df_text_cleaned.copy()
+    df_text_clean_dtypes_adjusted = df_text_comments_cleaned.copy()
 
     df_text_clean_dtypes_adjusted["session_date"] = pd.to_datetime(df_text_clean_dtypes_adjusted["session_date"], format= "%d.%m.%Y")
     df_text_clean_dtypes_adjusted["speaker_id"] = df_text_clean_dtypes_adjusted["speaker_id"].astype(str)
     df_text_clean_dtypes_adjusted["legaslative_period"] = df_text_clean_dtypes_adjusted["legaslative_period"].astype(int)
-    df_text_clean_dtypes_adjusted["session_nr"] = df_text_clean_dtypes_adjusted["session_nr"].astype(gtiint)
+    df_text_clean_dtypes_adjusted["session_nr"] = df_text_clean_dtypes_adjusted["session_nr"].astype(int)
 
 
     return df_text_clean_dtypes_adjusted
