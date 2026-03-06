8ac2b858ae3d228d9200d92cc4f2f82fd96210a3
lenni-1089 <rosenthal.lennart@gmail.com>
2026-03-06 09:47:56 +0000

    Add a check for already registered files (duplication) and raise custom DuplicateIngestionError previously defined.

--- ingestion/register_session_ingestion.py
+++ ingestion/register_session_ingestion.py
@@ -5,13 +5,21 @@ class DuplicateIngestionError(Exception):
 
     def __init__(self,message):
         super().__init__(message)
-    
 
+    
 def register_xml_fetch(spark:SparkSession,filepath:str, source_path:str):
     filename = filepath.split('/')[-1]
     legislative_period, session_nr = filename.replace(".xml","").split("_")
     legislative_period = int(legislative_period)
     session_nr = int(session_nr)
+
+    registered_sessions = spark.sql(f"""
+                                   SELECT * FROM bundestag_dev.bronze.raw_sessions
+                                   WHERE filename = '{filename}';
+                                   """)
+    
+    if registered_sessions.count() > 0:
+        raise DuplicateIngestionError(f"Session file {filename} was already registered {registered_sessions.count()} times.")
 
     spark.sql(f"""
                 INSERT INTO bundestag_dev.bronze.raw_sessions
