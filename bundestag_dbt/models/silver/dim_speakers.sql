43ab7cb381754d68cad8873be1847a89bf8eee30
Lennart Rosenthal <rosenthal.lennart@gmail.com>
2026-03-12 10:34:40 +0100

    Code clean up

--- bundestag_dbt/models/silver/dim_speakers.sql
+++ bundestag_dbt/models/silver/dim_speakers.sql
@@ -1,9 +1,6 @@
-WITH distinct_speakers AS(
-    SELECT DISTINCT speaker_id, name, lastname, role, party_affiliation
-    FROM {{ ref('stg_bundestag__speeches') }}
-)
-
-SELECT *
-FROM distinct_speakers
-WHERE speaker_id IN ('11004678','11004437','11004182')
-ORDER BY speaker_id
+SELECT DISTINCT speaker_id ,
+        name,
+        lastname,
+        party_affiliation,
+        role
+FROM {{ ref('stg_bundestag__speeches') }}
