80f015c5c5febaa48cf20db34c8ddb9fe9391406
Lennart Rosenthal <rosenthal.lennart@gmail.com>
2026-03-13 09:33:46 +0100

    Add ordering by legaslative period , session nr (newest to oldest), whithin session by speech id in chronological order of the session

--- bundestag_dbt/models/staging/stg_bundestag__speeches.sql
+++ bundestag_dbt/models/staging/stg_bundestag__speeches.sql
@@ -1,2 +1,6 @@
 SELECT *
 FROM {{source('bundestag', 'speeches_staging') }}
+
+-- even though ordering serves no real technical purpose
+-- it is helpful for debugging and testing to have the most recent sessions at the top of the table
+ORDER BY  legaslative_period DESC, session_nr DESC, speech_id ASC
